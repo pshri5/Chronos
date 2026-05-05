49e11fdebdb47ce5c577f801c498e4f0d95ff733
Shri <110012624+pshri5@users.noreply.github.com>
2026-05-05 20:46:50 +0530

    Add hashing and comparing passwords

--- Backend/src/models/user.model.ts
+++ Backend/src/models/user.model.ts
@@ -1,4 +1,6 @@
+import type { NextFunction } from "express";
 import mongoose,{Schema} from "mongoose";
+import bcrypt from "bcrypt"
 
 
 
@@ -23,3 +25,16 @@ const userSchema: Schema = new Schema({
 userSchema.index({ email: 1 });
 
 export const User = mongoose.model("User",userSchema)
+
+//Hashing password before saving
+userSchema.pre("save",async function (next) {
+    if(!this.isModified("password")) return next() //preventing rehashing of the password
+    
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+//comparing saved password and input password
+userSchema.methods.isPasswordCorrect = async function(inputPassword){
+   return await bcrypt.compare(inputPassword, this.password)
+}
