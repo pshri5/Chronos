9eb8125f010b075dc409944bb8f3f43b11b9aa83
Shri <110012624+pshri5@users.noreply.github.com>
2026-05-09 21:52:10 +0530

    Revert "fixing issues"
    This reverts commit 743d57e6e00da581d95b199776ea47eb5f227b66.

--- Backend/src/db/index.ts
+++ Backend/src/db/index.ts
@@ -5,22 +5,14 @@ import mongoose from "mongoose";
 Connect to mongodb via mongoose
 */
 
-let isConnected = false;
-
 export const connectDB = async() =>{
-    if (isConnected) {
-        console.log("Using existing database connection");
-        return;
-    }
-    
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`)
-        isConnected = connectionInstance.connections[0].readyState === 1;
         console.log(`Mongodb connected successfully! DB connection host: ${connectionInstance.connection.host}`)
         
     } catch (error: any) {
         console.log("Mongodb connection error",error)
-        throw new Error("Database connection failed");
+        process.exit(1)
     }
 }
 
